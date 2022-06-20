0129f4f4a7c69a5fcf6ce3bfa1bf7d08d672265e
David Goldfarb <goldfarb.dave@gmail.com>
2022-06-20 17:04:38 +0100

    Have working amortized SM (use table instead of pointers for nodes)

--- code/models/sequencememoizer.cpp
+++ code/models/sequencememoizer.cpp
@@ -24,6 +24,135 @@ double get_init_discount(std::size_t depth) {
     return g_discount_ar[idx];
 }
 template <std::size_t num_children>
+struct SMData {
+    using count_t = std::uint8_t;
+    using idx_t = std::size_t;
+    using IdxContext = Context<idx_t>;
+    using ProbAr = std::array<double, num_children>;
+    std::array<count_t, num_children> m_cs{};
+    std::array<count_t, num_children> m_ts{};
+    std::size_t m_ctot{};
+    std::size_t m_ttot{};
+    double m_d{};
+    std::array<std::size_t, num_children> children{};
+    std::size_t depth{};
+    ProbAr weight_probs(ProbAr const &parent_probs) const {
+        // See Eq1.
+        if (!m_ctot) {
+            return parent_probs;
+        }
+        ProbAr tmp;
+        for (int i =0; i < tmp.size(); ++i) {
+            tmp[i] = (m_cs[i] - m_d*m_ts[i] + m_d*m_ttot*parent_probs[i])/m_ctot;
+        }
+        return tmp;
+
+    }
+    ProbAr get_prior() const {
+        ProbAr ret;
+        std::fill(ret.begin(), ret.end(), 1.0/num_children);
+        return ret;
+    }
+    bool update_counts(idx_t sym, ProbAr const& parent_probs) {
+        // Unconditionally increment C
+        m_cs[sym] += 1;
+        m_ctot += 1;
+        auto const RESCALE_THRESHOLD = std::numeric_limits<uint8_t>::max();
+        if (m_cs[sym] == RESCALE_THRESHOLD) {
+            std::cout << "RESCALE" << std::endl;
+            auto new_total = 0;
+            std::transform(m_cs.begin(), m_cs.end(), m_cs.begin(),
+                           [this, &new_total](auto const &el) {
+                               // Divide by 2, round up
+                               auto nc =(el >> 1) + (el & 1);
+                               new_total += nc;
+                               return nc;
+                           });
+            m_ctot = new_total;
+        }
+
+        // Flip T, see old value. In Chinese Restaurant Parlance: we didn't make a new table
+        auto old_t = std::exchange(m_ts[sym], 1);
+        m_ttot += !old_t;
+        return old_t;
+    }
+    SMData(std::size_t depth_) :depth{depth_}, m_d{get_init_discount(depth_)} {
+        for (int i = 0 ; i < num_children; ++i) {
+            assert(m_cs[i] == 0);
+            assert(m_ts[i] == 0);
+            assert(children[i] ==0);
+        }
+    }
+};
+template <std::size_t num_children>
+class AmortSM {
+    std::vector<SMData<num_children>> m_vec;
+    std::size_t m_root{0};
+    std::size_t m_depth;
+    using idx_t = size_t;
+    using IdxContext = Context<idx_t>;
+    using ProbAr = std::array<double, num_children>;
+    struct IdxAndProb {
+        std::size_t idx;
+        ProbAr prob;
+    };
+    auto get_deepest_prob(IdxContext const &ctx) const {
+        auto idx = 0;
+        auto ret= m_vec[0].weight_probs(m_vec[0].get_prior());
+        for (IdxContext c = ctx; c; c.pop()) {
+            auto const child_idx = m_vec[idx].children[c.back()];
+            // Leave if can't make child
+            if (!child_idx) {
+                break;
+            }
+            ret = m_vec[child_idx].weight_probs(ret);
+            idx=child_idx;
+        }
+        return ret;
+    }
+    auto get_prob_path(IdxContext const &ctx) {
+        std::vector<IdxAndProb> cpps;
+        cpps.reserve(m_depth+1);
+        cpps.emplace_back(IdxAndProb{.idx=0, .prob=m_vec[0].get_prior()});
+        for (IdxContext c = ctx; c; c.pop()) {
+            // Note we don't form references to m_vec[idx] because we're resizing m_vec.
+            auto const parent_idx = cpps.back().idx;
+            // Make child if needed:
+            if (!m_vec[parent_idx].children[c.back()]) {
+                m_vec[parent_idx].children[c.back()] = m_vec.size();
+                m_vec.emplace_back(m_vec[parent_idx].depth+1);
+            }
+            auto child_idx = m_vec[parent_idx].children[c.back()];
+            // Push
+            cpps.emplace_back(IdxAndProb{.idx=child_idx,
+                                         .prob=m_vec[child_idx].weight_probs(cpps.back().prob)});
+        }
+        return cpps;
+    }
+public:
+    auto size() const {
+        return m_vec.size();
+    }
+    AmortSM(std::size_t depth) :m_depth{depth} {
+        m_vec.emplace_back(0);
+    }
+    ProbAr get_probs(IdxContext const &ctx) const {
+        return get_deepest_prob(ctx);
+    }
+    void learn(IdxContext const &ctx, idx_t sym) {
+        // idx, prob, pair.
+        auto cpps = get_prob_path(ctx);
+        // Work backwards, updating:
+        for (auto itr = cpps.crbegin(); itr != cpps.crend(); ++itr) {
+            if (m_vec[itr->idx].update_counts(sym, itr->prob)) {
+                break;
+            }
+        }
+    }
+};
+
+
+template <std::size_t num_children>
 class SMNode {
     using idx_t = size_t;
     using IdxContext = Context<idx_t>;
@@ -35,8 +164,8 @@ class SMNode {
     std::array<count_t, num_children> m_ts{};
     std::size_t m_ctot{};
     std::size_t m_ttot{};
-    std::size_t m_depth;
-    double m_d;
+    std::size_t m_depth{};
+    double m_d{};
     std::unique_ptr<SMNode> make_child() {
         return std::make_unique<SMNode>(m_depth+1);
     }
@@ -65,7 +194,7 @@ class SMNode {
     }
     ProbAr get_prior() const {
         ProbAr ret;
-        std::fill(ret.begin(), ret.end(), static_cast<double>(1)/num_children);
+        std::fill(ret.begin(), ret.end(), 1.0/num_children);
         return ret;
     }
     ProbAr get_probs_rec(IdxContext const &ctx, ProbAr const &parent_probs) const {
@@ -100,6 +229,7 @@ class SMNode {
         return old_t;
     }
     bool learn_down(IdxContext const &ctx, idx_t sym, ProbAr const &parent_probs) {
+        // Traverse
         if (ctx) {
             auto broke = get_child(ctx.back()).learn_down(ctx.popped(), sym, weight_probs(parent_probs));
             if (broke) return broke;
@@ -147,20 +277,68 @@ public:
         m_root.learn(view, idx);
         m_past_idxs.push_back(idx);
     }
-
 };
-
+template <typename AlphabetT>
+class SequenceMemoizerAmort {
+public:
+    using Alphabet = AlphabetT;
+private:
+    using count_t = std::size_t;
+    using idx_t = std::size_t;
+    using sym_t = typename Alphabet::sym_t;
+    MemoryDeque<idx_t> m_past_idxs;
+    AmortSM<Alphabet::size> m_sm;
+public:
+    Footprint footprint() const {
+        return {.num_nodes = m_sm.size(),
+                .node_size = sizeof(SMData<Alphabet::size>)};
+    }
+    auto get_probs() const {
+        return m_sm.get_probs(m_past_idxs.view());
+    }
+    SequenceMemoizerAmort (std::size_t depth): m_past_idxs{depth}, m_sm{depth} {}
+    void learn(sym_t sym) {
+        auto idx = Alphabet::to_idx(sym);
+        auto view = m_past_idxs.view();
+        m_sm.learn(view, idx);
+        m_past_idxs.push_back(idx);
+    }
+};
 
 #include "corpus.hpp"
 #include "model_utils.hpp"
 #include "ac.hpp"
 #include "limit_mem.hpp"
 #include "utils.hpp"
+// template <typename ModelCtorT>
+// void correctness_and_entropy_test(ModelCtorT ctor) {
+//     //static_assert(std::is_same_v<typename decltype(ctor())::Alphabet::sym_t,bit_t>);
+//     auto contents = load_file_in_memory(cantbry_name_to_path.at("fields.c"));
+//     BitVec compressed;
+//     {
+//         StreamingACEnc ac(compressed, ctor());
+//         for (auto const &sym: contents.bytes) {
+//             ac.encode(sym);
+//         }
+//     }
+//     std::cout << "Compression: " << contents.bits.size() << " -> " <<compressed.size() << std::endl;
+//     StreamingACDec ac(std::move(compressed), ctor());
+//     for (auto const &gt : contents.bytes) {
+//         if (ac.decode() != gt) {
+//             throw std::runtime_error{"Mismatch!"};
+//         }
+//     }
+//     auto res = entropy_of_model(contents.bytes, ctor());
+//     std::cout << "Entropy: " << res.H << std::endl;
+//     std::cout << "Size: " << res.model.footprint().mib() << "MiB" << std::endl;
+// }
+
 template <typename ModelCtorT>
 void entropy_test(ModelCtorT ctor) {
     // static_assert(std::is_same_v<typename decltype(ctor())::Alphabet::sym_t,bit_t>);
-    // auto contents = load_file_in_memory(cantbry_name_to_path.at("fields.c"));
-    auto contents = load_file_in_memory(cantbry_name_to_path.at("asyoulik.txt"));
+    auto contents = load_file_in_memory(cantbry_name_to_path.at("fields.c"));
+    // auto contents = load_file_in_memory(cantbry_name_to_path.at("asyoulik.txt"));
+
     auto res = [&]()
     {
         TimeSection ts("Do calc");
@@ -169,12 +347,20 @@ void entropy_test(ModelCtorT ctor) {
 
     std::cout << "Entropy: " << res.H << std::endl;
     std::cout << "bits/Byte: " << res.H / contents.bytes.size() << std::endl;
-    std::cout << "Size: " << res.model.footprint().mib() << "MiB" << std::endl;
+    std::cout << "Size: " << res.model.footprint().mib() << "MiB (" << res.model.footprint().num_nodes << ")" <<  std::endl;
 }
 int main() {
     limit_gb(3);
+
     entropy_test([]() {
         return SequenceMemoizer<ByteAlphabet>(12);
     });
+    entropy_test([]() {
+        return SequenceMemoizerAmort<ByteAlphabet>(12);
+    });
+
+    // correctness_and_entropy_test([]() {
+    //     return SequenceMemoizerAmort<ByteAlphabet>(8);
+    // });
     // auto sm = SequenceMemoizer<ByteAlphabet>(5);
 }
