77f63606838fd6ce2210d6f0c0ece0af7c1080e3
David Goldfarb <goldfarb.dave@gmail.com>
2022-06-20 18:03:03 +0100

    Adds calgary analysis

---
+++ code/scripts/calgary.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../harness/calgary.csv")
+# Add b/B
+df = df.join((df["Entropy"]/df["FSize"]).to_frame(name="b/B"))
+
+# Top 3 for each file
+for fn in np.unique(df["File"]):
+    print(fn)
+    ndf = df[df["File"] == fn]
+    print(ndf.sort_values(by='b/B').head(2))
