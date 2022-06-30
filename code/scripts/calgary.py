99f763672b25215995c495a0c14aa006a4c7cc65
David Goldfarb <goldfarb.dave@gmail.com>
2022-06-30 18:42:03 +0100

    Puts PPMD calgary results (hash-ppmd probably wrong?)

--- code/scripts/calgary.py
+++ code/scripts/calgary.py
@@ -26,13 +26,14 @@ for idx, fn in enumerate(fns):
     r,c = to_coords(idx)
     ax = axs[r,c]
     ndf = df[df["File"] == fn]
-    for meth,color in [ ("SM1PF", 'red'),  ("SMUKN", 'blue'),("CTW", 'green'),]: 
+    #for meth,color in [ ("SM1PF", 'red'),  ("SMUKN", 'blue'),("CTW", 'green'),]:
+    for meth, color in [("PPMDP", 'red'), ("SMUKN", "blue"), ("PPMDPFull", 'green')]:
         baseline = ndf[ndf["Meth"] == meth]
         hash_meths = ndf[ndf["Meth"] == f"Hash{meth}"].sort_values(by='MSize')
         amn_meths = ndf[ndf["Meth"] == f"Amnesia{meth}"].sort_values(by='MSize')
         lb_meths = ndf[ndf["Meth"] == f"LengthBucketHash{meth}"].sort_values(by='MSize')
         ax.plot(hash_meths["MSize"].values, hash_meths["b/B"], color=color, label=f"Stochastic-{meth} Compression Ratio")
-        ax.plot(amn_meths["MSize"].values, amn_meths["b/B"], color=color, linestyle='dashdot', label=f"Amnesia-{meth} Compression Ratio")
+        # ax.plot(amn_meths["MSize"].values, amn_meths["b/B"], color=color, linestyle='dashdot', label=f"Amnesia-{meth} Compression Ratio")
         # ax.plot(lb_meths["MSize"].values, lb_meths["b/B"], color="purple", label=f"Length Bucket Stochastic {meth} Compression Ratio")
         ax.axhline(y=baseline["b/B"].values[0], linestyle='dashed', color=color, label=f"{meth} Compression Ratio")
         ax.axvline(x=baseline["MSize"].values[0], linestyle='dotted', color=color, label=f"{meth} Histograms used")
