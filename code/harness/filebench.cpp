625063119a180e0438c4ac1c88e20bc85c9c4cff
David Goldfarb <goldfarb.dave@gmail.com>
2022-07-29 14:54:54 +0100

    Larger progp test run prepush

--- code/harness/filebench.cpp
+++ code/harness/filebench.cpp
@@ -17,7 +17,7 @@ std::map<std::string, std::function<std::vector<Task>()>> name_to_task_map = {
 
 int main(int argc, char *argv[]) {
     // limit_gb(5);
-    int const num_threads = std::thread::hardware_concurrency();
+    int const num_threads = 1; //std::thread::hardware_concurrency();
     std::cerr << "Executing on " << num_threads << " threads\n";
     Executor<LineItem> executor{num_threads};
 
@@ -34,6 +34,7 @@ int main(int argc, char *argv[]) {
     }();
     std::cout << LineItem::header() << std::endl;
     for (auto const &task: tasks) {
+        if (task.file_name != "progp") continue;
         auto wrap_task = [task, &executor](auto ctor){executor.push_back([task, ctor](){return run_task(task, ctor);});};
         // Convinience macros for more ergonomic typing. Could do XMacro, but it's nice to have comment-out-able list
 // #define pw(str, mod) wrap_task(plain_factory<mod>(str))
@@ -56,18 +57,19 @@ int main(int argc, char *argv[]) {
                                  STRINGIFY(Pure1Hash##str##Model),  \
                                  1UL<<log_tab_size))
 #define pure2(str) wrap_task(tab_factory<Pure2Hash##str##Model>(    \
-                                 STRINGIFY(Pure2Hash##str##Model),  \
+                                 STRINGIFY(Pure2Hash##str),  \
                                  1UL<<log_tab_size))
         // plain(CTW);
         // plain(SM1PF);
         // plain(SMUKN);
         // plain(PPMDP);
         // plain(PPMDPFull);
-        for (int log_tab_size = 7; log_tab_size < 21; ++log_tab_size) {
+        // for (int log_tab_size = 7; log_tab_size < 8; ++log_tab_size) {
+        for (int log_tab_size = 20; log_tab_size < 30; ++log_tab_size) {
             // hash(CTW);
             // hash(SM1PF);
             // hash(SMUKN);
-            // hash(PPMDP);
+            hash(PPMDP);
             // hash(PPMDPFull);
             // fnvhash(CTW);
             // fnvhash(SM1PF);
@@ -79,25 +81,25 @@ int main(int argc, char *argv[]) {
             // amnesia(SMUKN);
             // amnesia(PPMDP);
             // amnesia(PPMDPFull);
-            pure0(CTW);
-            pure0(SM1PF);
-            pure0(SMUKN);
-            pure0(PPMDP);
-            pure0(PPMDPFull);
-            if (log_tab_size > 8) {
-                pure1(CTW);
-                pure1(SM1PF);
-                pure1(SMUKN);
-                pure1(PPMDP);
-                pure1(PPMDPFull);
-            }
-            if (log_tab_size > 16) {
-                pure2(CTW);
-                pure2(SM1PF);
-                pure2(SMUKN);
-                pure2(PPMDP);
-                pure2(PPMDPFull);
-            }
+            // pure0(CTW);
+            // pure0(SM1PF);
+            // pure0(SMUKN);
+            // pure0(PPMDP);
+            // pure0(PPMDPFull);
+            // if (log_tab_size > 8) {
+            //     pure1(CTW);
+            //     pure1(SM1PF);
+            //     pure1(SMUKN);
+            //     pure1(PPMDP);
+            //     pure1(PPMDPFull);
+            // }
+            // if (log_tab_size > 16) {
+            //     pure2(CTW);
+            //     pure2(SM1PF);
+            //     pure2(SMUKN);
+            //     pure2(PPMDP);
+            //     pure2(PPMDPFull);
+            // }
         }
 #undef pure2
 #undef pure1
